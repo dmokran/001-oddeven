e8a2ba9a2cc730974bdecc8e3c9fc821b1e7b55c
dmokran <dmokran@gmail.com>
2016-09-09 14:23:53 +0200

    with cProfile example

--- oddeven.py
+++ oddeven.py
@@ -1,5 +1,35 @@
-import string
+# -------------
+# User Instructions
+#
+# Complete the fill_in(formula) function by adding your code to
+# the two places marked with ?????. 
 
-transTable = str.maketrans('abc', '123')
+import string, re, itertools, cProfile
 
-print(transTable)
+
+def solve(formula):
+    """Given a formula like 'ODD + ODD == EVEN', fill in digits to solve it.
+    Input formula is a string; output is a digit-filled-in string or None."""
+    for f in fill_in(formula):
+        if valid(f):
+            return f
+
+
+def fill_in(formula):
+    "Generate all possible fillings-in of letters in formula with digits."
+    letters = ''.join(set([formula[i] for i in range(len(formula)) if formula[i].isalpha()]))  # should be a string
+    for digits in itertools.permutations('1234567890', len(letters)):
+        table = str.maketrans(letters, ''.join(digits))
+        yield formula.translate(table)
+
+
+def valid(f):
+    """Formula f is valid if and only if it has no 
+    numbers with leading zero, and evals true."""
+    try:
+        return not re.search(r'\b0[0-9]', f) and eval(f) is True
+    except ArithmeticError:
+        return False
+
+
+cProfile.run('print(solve("ODD + ODD == EVEN"))')
